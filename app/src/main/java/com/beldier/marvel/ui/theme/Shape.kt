9cfed9488dcc1d95bc163ed31f7756b97de0181d
Alvaro Yapu <alvaro.yapu.cossio@gmail.com>
2022-11-27 11:41:05 -0400

    Change shapes of material elements

--- app/src/main/java/com/beldier/marvel/ui/theme/Shape.kt
+++ app/src/main/java/com/beldier/marvel/ui/theme/Shape.kt
@@ -1,11 +1,13 @@
 package com.beldier.marvel.ui.theme
 
+import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
+//    small = CutCornerShape(4.dp),
     small = RoundedCornerShape(4.dp),
-    medium = RoundedCornerShape(4.dp),
-    large = RoundedCornerShape(0.dp)
+    medium = RoundedCornerShape(16.dp),
+    large = RoundedCornerShape(8.dp)
 )
